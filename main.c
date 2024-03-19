00fbdbfdda018304a66d88ab7ce2a62be884a82c
JuGerez <100386098+JuGerez@users.noreply.github.com>
2024-03-19 19:49:30 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,20 +4,35 @@
 #include <time.h>
 #include "pila.h"
 
-void cargaPilaRandom(Pila* k);
-void muestraPilaRandom (Pila k);
+void cargaPilaRandom(Pila*k);
+void pasaPila(Pila* k, Pila*l);
+int cuentaElementosPila(Pila l);
+int sumaElementosPila(Pila l);
 
 
 int main()
 {
-    Pila tempLondon;
-    inicpila (&tempLondon);
+    Pila origen, destino;
+    inicpila (&origen);
+    inicpila (&destino);
 
-    cargaPilaRandom(&tempLondon);
+    cargaPilaRandom(&origen);
 
-    printf("Muestra temperaturas de Londres en verano: \n");
-    //Las temperaturas promedio en verano son entre 10 y 23°C.
-    muestraPilaRandom(tempLondon);
+    printf("Pila origen:\n");
+    mostrar(&origen);
+
+    int cantidad=cuentaElementosPila(origen);
+    printf(" Cantidad elementos: %d \n", cantidad);
+
+    int total=sumaElementosPila(origen);
+    printf(" Suma elementos: %d \n", total);
+
+    pasaPila(&origen, &destino);
+    printf("Pila origen final:\n");
+    mostrar(&origen);
+    printf("Pila destino:\n");
+    mostrar(&destino);
+
 
     return 0;
 }
@@ -27,21 +42,44 @@ void cargaPilaRandom(Pila* k)
     int numero;
     srand(time(NULL));
 
-    for (int i = 1; i < 50; i++)
-    {
-        numero = rand()%23+10;
+    for (int i = 0; i < 50; i++){
+        numero = rand()%100+1;
         apilar(k, numero);
     }
 }
 
-void muestraPilaRandom(Pila k)
-{
+void pasaPila(Pila*k, Pila*l){
+    while(!pilavacia(k)){
+    apilar(l, desapilar(k));
+    }
+}
+
+int cuentaElementosPila(Pila l){
+    int contador = 0;
     Pila aux;
     inicpila(&aux);
 
-    while(!pilavacia(&k))
-    {
-        printf("%d - ", tope(&k));
-        apilar(&aux, desapilar(&k));
+    while(!pilavacia(&l)){
+        apilar(&aux, desapilar(&l));
+        contador++;
     }
+    return contador;
 }
+
+int sumaElementosPila(Pila l){
+    int acumulador = 0;
+    Pila aux;
+    inicpila(&aux);
+
+    while(!pilavacia(&l)){
+        int topepila;
+        topepila = tope(&l);
+        acumulador = acumulador + topepila;
+        apilar(&aux, desapilar(&l));
+    }
+    return acumulador;
+}
+
+
+
+//
